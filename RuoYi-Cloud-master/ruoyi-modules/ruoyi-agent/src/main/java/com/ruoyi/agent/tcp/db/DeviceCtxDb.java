f1f0998744a251dbb251200133b18dd00dbb0b93
Zhou7 <Zhou730428@outlook.com>
2025-01-23 22:15:40 +0800

    netty+websocket

---
+++ RuoYi-Cloud-master/ruoyi-modules/ruoyi-agent/src/main/java/com/ruoyi/agent/tcp/db/DeviceCtxDb.java
@@ -0,0 +1,9 @@
+package com.ruoyi.agent.tcp.db;
+
+import com.ruoyi.agent.tcp.domain.TcpQueue;
+
+import java.util.Vector;
+
+public class DeviceCtxDb {
+    public static Vector<TcpQueue> queueList =new Vector<>();
+}
