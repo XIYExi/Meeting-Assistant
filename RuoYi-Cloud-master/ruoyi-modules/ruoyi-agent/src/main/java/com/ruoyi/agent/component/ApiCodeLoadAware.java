ddaefc9a1e42e496e6e8184b1a42f058f9f8571e
Zhou7 <Zhou730428@outlook.com>
2025-01-23 22:14:49 +0800

    netty bean加载

---
+++ RuoYi-Cloud-master/ruoyi-modules/ruoyi-agent/src/main/java/com/ruoyi/agent/component/ApiCodeLoadAware.java
@@ -0,0 +1,18 @@
+package com.ruoyi.agent.component;
+
+import com.ruoyi.agent.constant.ConstValuePool;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class ApiCodeLoadAware implements ApplicationContextAware {
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ConstValuePool.PROXY_HENG_NAO_CLIENT = WebClient.builder()
+                .baseUrl("")
+                .build();
+    }
+}
