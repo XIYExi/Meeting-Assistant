30465cdd8b34800fe31f0ede131ba568a9c0156f
Zhou7 <Zhou730428@outlook.com>
2025-01-23 22:14:38 +0800

    请求接口

--- RuoYi-Cloud-master/ruoyi-modules/ruoyi-agent/src/main/java/com/ruoyi/agent/controller/ChatController.java
+++ RuoYi-Cloud-master/ruoyi-modules/ruoyi-agent/src/main/java/com/ruoyi/agent/controller/ChatController.java
@@ -2,6 +2,10 @@ package com.ruoyi.agent.controller;
 
 import com.ruoyi.agent.component.HNComponent;
 import com.ruoyi.agent.constant.AgentRequestPath;
+import com.ruoyi.agent.entity.ChatReq;
+import com.ruoyi.agent.service.AgentChatService;
+import com.ruoyi.agent.service.IMessagesLogsService;
+import com.ruoyi.agent.service.impl.MessagesLogsServiceImpl;
 import com.ruoyi.agent.utils.SignUtils;
 import com.ruoyi.common.core.web.domain.AjaxResult;
 import lombok.AllArgsConstructor;
@@ -9,10 +13,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -30,6 +31,8 @@ public class ChatController {
 
     private static final String agentId = "bbbb5af3-b5c7-4483-8251-009e85e53e24";
 
+    private AgentChatService agentChatService;
+
     @GetMapping("/chat")
     public AjaxResult chat(@RequestParam(value = "question") String question) throws Exception {
         String executeUrl = AgentRequestPath.generateRequestPath(AgentRequestPath.agentExecuteUrl);
@@ -40,7 +43,7 @@ public class ChatController {
         headers.add("sign", SignUtils.getSign(hnComponent.getAppKey(), hnComponent.getAppSecret()));
 
         Map<String, Object> requestBody = new HashMap<>();
-        requestBody.put("sid", UUID.randomUUID().toString());
+        requestBody.put("sid", "a5778aec-1109-47d2-b337-c7014992870b");
         requestBody.put("id", agentId);
         requestBody.put("input", question);
         HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
@@ -49,6 +52,20 @@ public class ChatController {
         return AjaxResult.success(responseEntity);
     }
 
+
+    @PostMapping("/flux")
+    @ResponseBody
+    public AjaxResult flux(@RequestBody ChatReq req) {
+        System.err.println(req.getText());
+        AjaxResult ajax = AjaxResult.success("");
+        try {
+            String msg = agentChatService.sendFluxMsg(req.getUid(), req.getText());
+            ajax = AjaxResult.success(msg);
+        } catch (Exception e) {
+            ajax = AjaxResult.error(e.getMessage());
+        }
+        return ajax;
+    }
 
 
 }
