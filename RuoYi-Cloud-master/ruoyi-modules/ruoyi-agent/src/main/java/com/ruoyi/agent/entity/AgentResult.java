54e426c4bb4fe44fdf3ecbfec34fa6a9398f845f
Zhou7 <Zhou730428@outlook.com>
2025-01-23 22:13:58 +0800

    恒脑AgentResult接口

---
+++ RuoYi-Cloud-master/ruoyi-modules/ruoyi-agent/src/main/java/com/ruoyi/agent/entity/AgentResult.java
@@ -0,0 +1,28 @@
+package com.ruoyi.agent.entity;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class AgentResult {
+
+    private String model;
+
+    private String type;
+
+    private String form;
+
+    private String name;
+
+    private String timestamp;
+
+    private String message_id;
+
+    private String content;
+
+    private Map<String, Object> results;
+
+
+
+}
