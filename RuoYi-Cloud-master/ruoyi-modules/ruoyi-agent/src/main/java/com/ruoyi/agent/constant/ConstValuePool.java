e268880c009ff8646a360f968291e9856516cd5e
Zhou7 <Zhou730428@outlook.com>
2025-01-23 22:15:19 +0800

    netty server池

---
+++ RuoYi-Cloud-master/ruoyi-modules/ruoyi-agent/src/main/java/com/ruoyi/agent/constant/ConstValuePool.java
@@ -0,0 +1,9 @@
+package com.ruoyi.agent.constant;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class ConstValuePool {
+    // 恒脑代理客户端
+    public static WebClient PROXY_HENG_NAO_CLIENT = null;
+}
