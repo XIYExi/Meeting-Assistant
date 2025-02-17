bf516982b2e3d17f3a8598a2312ecdf2be2f6a02
Zhou7 <Zhou730428@outlook.com>
2025-02-17 08:52:34 +0800

    0217早

--- meeting-wx/pages/schedule/detail/agenda.vue
+++ meeting-wx/pages/schedule/detail/agenda.vue
@@ -38,42 +38,7 @@ export default {
   },
   data() {
     return {
-      agendaItems: [
-        {
-          time: "08:30 - 09:00",
-          title: "媒体签到、暖场视频",
-          people: []
-        },
-        {
-          time: "09:00 - 09:05",
-          title: "主持人开场",
-          people: []
-        },
-        {
-          time: "09:05 - 09:15",
-          title: "领导致辞",
-          people: [
-            { name: "范渊", position: "安恒信息董事长" }
-          ]
-        },
-        {
-          time: "09:15 - 09:20",
-          title: "发布仪式：安恒信息2024新一代产品全景图（恒脑2.0驱动全栈产品核心能力突破）",
-          people: [
-            { name: "范渊", position: "安恒信息董事长" },
-            { name: "刘博", position: "安恒信息CTO" },
-            { name: "袁明坤", position: "安恒信息CSO" },
-            { name: "王欣", position: "安恒信息研究院院长" }
-          ]
-        },
-        {
-          time: "09:20 - 09:40",
-          title: "AI如何“加”网络安全",
-          people: [
-            { name: "谭晓生", position: "北京赛博英杰科技有限公司创始人" }
-          ]
-        }
-      ]
+      agendaItems: []
     };
   },
   computed: {
@@ -83,7 +48,7 @@ export default {
       const agendas = raw_agendas.map(item => {
         const content = item.content;
         const time = formatTimeRange(item.beginTime, item.endTime);
-        console.log(time)
+        //console.log(time)
         let meta = null;
         if (item.meta!==null) {
           meta = this.formatMetaString(item.meta);
