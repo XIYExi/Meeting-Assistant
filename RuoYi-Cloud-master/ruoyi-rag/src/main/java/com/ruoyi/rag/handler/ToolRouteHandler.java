4a5ba2f2efcf73a1027e6c06379d2bf98f3402ef
Zhou7 <Zhou730428@outlook.com>
2025-03-04 16:00:06 +0800

    写错地了。。。

--- RuoYi-Cloud-master/ruoyi-rag/src/main/java/com/ruoyi/rag/handler/ToolRouteHandler.java
+++ RuoYi-Cloud-master/ruoyi-rag/src/main/java/com/ruoyi/rag/handler/ToolRouteHandler.java
@@ -15,14 +15,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 
-/**
- * 工具链：【Action操作】
- * 此操作目的是让Model可以接入数据库，统计算计Embedding到Milvus获取匹配的项目，并通过解析id和type获取对应的数据并返回
- *
- * 流程如下：
- * 1. 调用EmbeddingMode计算embedding -> 2. Milvus向量库相似度计算 -> 3. 选择最为匹配的数据，解析id和dbType（来自哪个数据库） ->
- * 4. feign调用ruoyi-meeting获取对应的数据 -> 5. 获取之后调用ws发送数据，此阶段打上“thinking”标志 -> 6. 最后整合，再送一次大模型总结输入最后结果
- */
+
 @Component
 public class ToolRouteHandler implements ToolSimpleHandler {
 
@@ -33,22 +26,6 @@ public class ToolRouteHandler implements ToolSimpleHandler {
 
     @Override
     public void handler(String content) {
-        // TODO content到底是什么格式呢？先按单个字符算吧
-        Response<Embedding> listResponse = domesticEmbeddingModel.embed(content);
-        Embedding embeddings = listResponse.content();
-        List<Float> features = embeddings.vectorAsList();
-
-        List<?> meeting = milvus.searchByFeature("meeting_home", features);
-
-        for (int i = 0; i < 2; ++i) {
-            Long composeId = (Long)meeting.get(i);
-            long originalId = IdGenerator.getOriginalId((Long) composeId);
-            boolean isMeeting = IdGenerator.isMeeting((Long) composeId);
-            int dbType = isMeeting ? 1 : 2;
-
-            // 远程meeting 获取信息
-
-        }
 
     }
 }
