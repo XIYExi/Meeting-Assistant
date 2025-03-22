2ea656b20a3833818215f64c0308bf192dd2a65a
Zhou7 <Zhou730428@outlook.com>
2025-03-22 21:56:38 +0800

    im集成到agent里面了，但是还需要改flux

--- RuoYi-Cloud-master/ruoyi-rag/src/main/java/com/ruoyi/rag/handler/AgentChatMsgHandler.java
+++ RuoYi-Cloud-master/ruoyi-rag/src/main/java/com/ruoyi/rag/handler/AgentChatMsgHandler.java
@@ -1,8 +1,11 @@
 package com.ruoyi.rag.handler;
 
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.ruoyi.common.core.web.domain.AjaxResult;
 import com.ruoyi.common.entity.im.ImMsgBody;
 import com.ruoyi.common.entity.im.MessageDTO;
+import com.ruoyi.rag.config.ImMsgBizCodeEnum;
 import com.ruoyi.rag.declare.SimpleMsgHandler;
 import com.ruoyi.router.api.RemoteRouterService;
 import org.slf4j.Logger;
@@ -28,7 +31,13 @@ public class AgentChatMsgHandler implements SimpleMsgHandler {
         Integer roomId = messageDTO.getRoomId();
         List<ImMsgBody> imMsgBodyList = new ArrayList<>();
         // TODO 需要改造
+        ImMsgBody respMsg = new ImMsgBody();
+        respMsg.setUserId(imMsgBody.getUserId());
+        respMsg.setAppId(imMsgBody.getAppId());
+        respMsg.setBizCode(ImMsgBizCodeEnum.Agent_Chat_Code.getCode());
+        respMsg.setData(com.alibaba.fastjson.JSON.toJSONString(messageDTO));
 
-
+        AjaxResult ajaxResult =remoteRouterService.sendMsg(imMsgBody.getUserId(), JSONObject.toJSONString(respMsg));
+        logger.info("rpc send msg to router server result: {}", ajaxResult);
     }
 }
