04ce4a9983374b7e621ddd014214328cdbb102db
Zhou7 <Zhou730428@outlook.com>
2025-03-23 12:41:46 +0800

    fix:FallbackFactory需要换包！

--- RuoYi-Cloud-master/ruoyi-api/ruoyi-api-im/src/main/java/com/ruoyi/im/api/factory/RemoteImFallbackFactory.java
+++ RuoYi-Cloud-master/ruoyi-api/ruoyi-api-im/src/main/java/com/ruoyi/im/api/factory/RemoteImFallbackFactory.java
@@ -5,18 +5,21 @@ import com.ruoyi.common.entity.im.ImMsgBody;
 import com.ruoyi.im.api.RemoteImService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.openfeign.FallbackFactory;
+// import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.stereotype.Component;
+import reactivefeign.FallbackFactory;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Component
 public class RemoteImFallbackFactory  implements FallbackFactory<RemoteImService> {
 
     private static final Logger log = LoggerFactory.getLogger(RemoteImFallbackFactory.class);
+
     @Override
-    public RemoteImService create(Throwable throwable) {
+    public RemoteImService apply(Throwable throwable) {
         log.error("会议服务调用失败:{}", throwable.getMessage());
         return new RemoteImService() {
 
@@ -25,10 +28,20 @@ public class RemoteImFallbackFactory  implements FallbackFactory<RemoteImService
                 return Mono.just(AjaxResult.error("Reactive OpenFeign Rpc Error: " + throwable.getMessage()));
             }
 
-            @Override
-            public AjaxResult batchRpc(List<ImMsgBody> imMsgBodyList) {
-                return AjaxResult.error("im batch rpc调用失败");
-            }
+//            @Override
+//            public AjaxResult batchRpc(List<ImMsgBody> imMsgBodyList) {
+//                return AjaxResult.error("im batch rpc调用失败");
+//            }
         };
+    }
+
+    @Override
+    public <V> Function<V, RemoteImService> compose(Function<? super V, ? extends Throwable> before) {
+        return FallbackFactory.super.compose(before);
+    }
+
+    @Override
+    public <V> Function<Throwable, V> andThen(Function<? super RemoteImService, ? extends V> after) {
+        return FallbackFactory.super.andThen(after);
     }
 }
--- RuoYi-Cloud-master/ruoyi-api/ruoyi-api-router/src/main/java/com/ruoyi/router/api/factory/RemoteRouterFallbackFactory.java
+++ RuoYi-Cloud-master/ruoyi-api/ruoyi-api-router/src/main/java/com/ruoyi/router/api/factory/RemoteRouterFallbackFactory.java
@@ -5,18 +5,21 @@ import com.ruoyi.router.api.RemoteRouterService;
 import com.ruoyi.common.core.web.domain.AjaxResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.openfeign.FallbackFactory;
+// import org.springframework.cloud.openfeign.FallbackFactory;
 import org.springframework.stereotype.Component;
+import reactivefeign.FallbackFactory;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Component
 public class RemoteRouterFallbackFactory implements FallbackFactory<RemoteRouterService> {
 
     private static final Logger log = LoggerFactory.getLogger(RemoteRouterFallbackFactory.class);
+
     @Override
-    public RemoteRouterService create(Throwable throwable) {
+    public RemoteRouterService apply(Throwable throwable) {
         log.error("im router服务调用失败:{}", throwable.getMessage());
         return new RemoteRouterService() {
             @Override
@@ -24,10 +27,20 @@ public class RemoteRouterFallbackFactory implements FallbackFactory<RemoteRouter
                 return Mono.just(AjaxResult.error("Reactive OpenFeign Rpc Error: " + throwable.getMessage()));
             }
 
-            @Override
-            public AjaxResult batchSendMsg(List<ImMsgBody> imMsgBody) {
-                return AjaxResult.error("im router send batch msgs error");
-            }
+//            @Override
+//            public AjaxResult batchSendMsg(List<ImMsgBody> imMsgBody) {
+//                return AjaxResult.error("im router send batch msgs error");
+//            }
         };
+    }
+
+    @Override
+    public <V> Function<V, RemoteRouterService> compose(Function<? super V, ? extends Throwable> before) {
+        return FallbackFactory.super.compose(before);
+    }
+
+    @Override
+    public <V> Function<Throwable, V> andThen(Function<? super RemoteRouterService, ? extends V> after) {
+        return FallbackFactory.super.andThen(after);
     }
 }
